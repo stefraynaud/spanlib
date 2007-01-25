c51282caca6e3b9679951672dbc518849b9b907f
stefraynaud <stephane.raynaud@gmail.com>
2007-01-25 15:25:02 +0000

    Forgot a new file

---
+++ src/spanlib_lapack95.F90
@@ -0,0 +1,34 @@
+! File: spanlib_lapack95.F90
+!
+! This file is part of the SpanLib library.
+! Copyright (C) 2006  Stephane Raynaud
+! Contact: stephane dot raynaud at gmail dot com
+!
+! This library is free software; you can redistribute it and/or
+! modify it under the terms of the GNU Lesser General Public
+! License as published by the Free Software Foundation; either
+! version 2.1 of the License, or (at your option) any later version.
+
+! This library is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! Lesser General Public License for more details.
+!
+! You should have received a copy of the GNU Lesser General Public
+! License along with this library; if not, write to the Free Software
+! Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+module spanlib_lapack95
+
+	! Precision
+	use la_precision
+
+	! Subroutines
+	use f95_lapack, only: &
+		& syev  => la_syev, &
+		& syevd => la_syevd, &
+		& gesvd => la_gesvd, &
+		& gesdd => la_gesdd
+
+end module spanlib_lapack95
+
