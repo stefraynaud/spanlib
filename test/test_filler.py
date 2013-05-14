acbe18ab94846cf7149252f9e92fa5ee80d59bc4
stefraynaud <stephane.raynaud@gmail.com>
2013-05-14 16:05:00 +0000

    test filler

--- test/test_filler.py
+++ test/test_filler.py
@@ -14,8 +14,30 @@ import pylab as P
 
 class TestSequenceFunctions(unittest.TestCase):
 
-#    def test_fill_simple(self):
-#        """Test hole filling and forecast estimation with a single variable"""
+    def test_fill_simple(self):
+        """Test hole filling and forecast estimation with a single variable"""
+        # Init
+        nt = 500
+        tmax = 80.
+        ref = npy.ma.sin(npy.linspace(0., tmax, nt))+10. # period = 2pi
+        ref = npy.ma.resize(ref, (3, nt)).T
+        ref[:, 1] = npy.ma.masked
+        withholes = ref.copy()
+#        withholes[200:215] = npy.ma.masked
+#        withholes[200:415] = npy.ma.masked
+#        withholes[480:] = npy.ma.masked
+        withholes[380:] = npy.ma.masked
+        # Fill
+        filled = Filler(withholes, loglevel='debug')
+        print 'plot'
+        P.plot(filled.filtered[:, 0], 'r')
+#        P.plot(withholes[:, 0], 'b')
+        P.show()
+        self.assertTrue(npy.allclose(filled.filtered.filled()[200:202,0], 
+            npy.array([10.61948956,  10.74003157])))
+        
+#    def test_fill_double(self):
+#        """Test hole filling and forecast estimation with a pair of variables"""
 #        # Init
 #        nt = 500
 #        tmax = 80.
@@ -26,31 +48,12 @@ class TestSequenceFunctions(unittest.TestCase):
 #        withholes[200:215] = npy.ma.masked
 #        withholes[480:] = npy.ma.masked
 #        # Fill
-#        filled = Filler(withholes)
-#        self.assertTrue(npy.allclose(filled.filtered.filled()[200:202,0], 
-#            npy.array([10.61948956,  10.74003157])))
-##        P.plot(filled.filtered[:, 0], 'r')
+#        filled = Filler([withholes, withholes*100])
+##        P.plot(filled.filtered[0][:, 0], 'r')
 ##        P.plot(withholes[:, 0], 'b')
 ##        P.show()
-        
-    def test_fill_double(self):
-        """Test hole filling and forecast estimation with a pair of variables"""
-        # Init
-        nt = 500
-        tmax = 80.
-        ref = npy.ma.sin(npy.linspace(0., tmax, nt))+10. # period = 2pi
-        ref = npy.ma.resize(ref, (3, nt)).T
-        ref[:, 1] = npy.ma.masked
-        withholes = ref.copy()
-        withholes[200:215] = npy.ma.masked
-        withholes[480:] = npy.ma.masked
-        # Fill
-        filled = Filler([withholes, withholes*100])
-#        P.plot(filled.filtered[0][:, 0], 'r')
-#        P.plot(withholes[:, 0], 'b')
-#        P.show()
-        self.assertTrue(npy.allclose(filled.filtered[0].filled()[200:202,0], 
-            npy.array([10.61834159,  10.73914392])))
+#        self.assertTrue(npy.allclose(filled.filtered[0].filled()[200:202,0], 
+#            npy.array([10.61834159,  10.73914392])))
         
 if __name__ == '__main__':
     unittest.main()
